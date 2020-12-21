ed17db2ac77de172a92a6245869ee90cde8ff293
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-21 16:45:15 +0900

    レシピ投稿機能実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,3 +1,21 @@
 class RecipesController < ApplicationController
+  before_action :authenticate_user!
   
+  def new
+    @recipe = Recipe.new
+  end
+
+  def create
+    @recipe = Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def recipe_params
+    params.require(:recipe).permit(:name, :description, :ingredient).merge(user_id: current_user.id)
+  end
 end
