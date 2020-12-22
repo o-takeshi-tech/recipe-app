a587816406bfe8c6a6c98fde027fc1e92969afed
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-22 19:14:35 +0900

    ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'recipes#index'
-  resources :recipes, only: %i[index new create]
+  resources :recipes, only: %i[index new create show]
   resources :ingredients, only: %i[new create]
 end
