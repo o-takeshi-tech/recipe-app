ebe7b0173eff42d6ec0aab0eeddcf4cffd8298c5
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-25 19:47:22 +0900

    テストコードの修正

--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -5,24 +5,38 @@ RSpec.describe Recipe, type: :model do
     before do 
       @ingredient = FactoryBot.create(:ingredient)
       @recipe = FactoryBot.build(:recipe)
-      @recipe.recipe_ingredients.build
+      @recipe.ingredient_ids = @ingredient.id     
     end 
     context 'レシピ登録ができる時' do
-      it 'name,description,image,userが揃えば保存できる' do
-        binding.pry
+      it 'name,description,image,user,ingredient_idsが揃えば保存できる' do
         expect(@recipe).to be_valid
       end
     end
     context 'レシピ登録ができない時' do
       it 'nameが空だと保存できない' do
+        @recipe.name = nil
+        @recipe.valid?
+        expect(@recipe.errors.full_messages).to include("Name can't be blank")
       end
       it 'descriptionが空だと保存できない' do
+        @recipe.description = nil
+        @recipe.valid?
+        expect(@recipe.errors.full_messages).to include("Description can't be blank")
       end
-      it 'recipe_ingredientsが空だと保存できない' do
+      it 'ingredient_idsが空だと保存できない' do
+        @recipe.ingredient_ids = nil
+        @recipe.valid?
+        expect(@recipe.errors.full_messages).to include("Recipe ingredient ids can't be blank")
       end
       it 'imageが空だと保存できない' do
+        @recipe.image = nil
+        @recipe.valid?
+        expect(@recipe.errors.full_messages).to include("Image can't be blank")
       end
       it 'userが紐付いていないと保存できない' do
+        @recipe.user = nil
+        @recipe.valid?
+        expect(@recipe.errors.full_messages).to include("User must exist")
       end
     end
 
