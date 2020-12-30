fe2bd2ffc77bfc5343b2a1909682fd3f5729a4dd
o-takeshi-tech <takeshi.film@gmail.com>
2020-12-30 18:27:29 +0900

    jsで非同期の実装

--- app/javascript/search.js
+++ app/javascript/search.js
@@ -1,1 +1,32 @@
-console.log("読み込み")
+function search (){
+  const inputElement = document.getElementById("keyword");
+  inputElement.addEventListener("keyup", () => {
+    const keyword = document.getElementById("keyword").value;
+    const XHR = new XMLHttpRequest();
+    XHR.open("GET", `recipes/search/?keyword=${keyword}`, true);
+    XHR.responseType = "json";
+    XHR.send();
+
+    XHR.onload = () => {
+      const searchResult = document.getElementById("search-result");
+      searchResult.innerHTML = "";
+      if (XHR.response){
+        const ingredientName = XHR.response.keyword;
+        ingredientName.forEach((ingredient) =>{
+          const childElement = document.createElement("div");
+          childElement.setAttribute("class", "child");
+          childElement.setAttribute("id", ingredient.id);
+          childElement.innerHTML = ingredient.ingredient_name
+          searchResult.appendChild(childElement);
+          const clickElement = document.getElementById(ingredient.id);
+          clickElement.addEventListener("click", () => {
+            document.getElementById("keyword").value = clickElement.textContent;
+            clickElement.remove();
+          });
+        });
+      };
+    }
+  })
+};
+
+window.addEventListener("load", search)
