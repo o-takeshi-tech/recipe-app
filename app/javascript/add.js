748c5543ad7da58ea51fff01e800d3846b57aaee
o-takeshi-tech <takeshi.film@gmail.com>
2021-01-25 18:54:51 +0900

    jsの変更

--- app/javascript/add.js
+++ app/javascript/add.js
@@ -2,7 +2,7 @@ function add(){
   const buttonPlus = document.getElementById("button_plus");
   
   buttonPlus.addEventListener("click", () => {
-    const form = document.querySelectorAll(".nested-fields");
+    const form = document.querySelectorAll(".input");
     const formNumber = form.length - 1;
     form[formNumber].insertAdjacentHTML("afterend", ingredient);
   });
--- app/javascript/destroy.js
+++ app/javascript/destroy.js
@@ -2,7 +2,7 @@ function destroy (){
   const buttonDelete = document.getElementById("button_delete");
   
   buttonDelete.addEventListener("click", () => {
-    const form = document.querySelectorAll(".nested-fields");
+    const form = document.querySelectorAll(".input");
     const formNumber = form.length - 1;
     if (formNumber == 0){
       return null 
