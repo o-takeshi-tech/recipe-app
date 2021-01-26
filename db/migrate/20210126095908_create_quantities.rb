2f10c8126ffce9ba31cd82165ba49b85504f7e30
o-takeshi-tech <takeshi.film@gmail.com>
2021-01-26 19:08:29 +0900

    テーブル変更

--- db/migrate/20210126095908_create_quantities.rb
+++ db/migrate/20210126095908_create_quantities.rb
@@ -1,6 +1,7 @@
-class CreateRecipeIngredients < ActiveRecord::Migration[6.0]
+class CreateQuantities < ActiveRecord::Migration[6.0]
   def change
-    create_table :recipe_ingredients do |t|
+    create_table :quantities do |t|
+      t.string        :quantity,     null: false
       t.references    :recipe,       null: false, foreign_key: true
       t.references    :ingredient,   null: false, foreign_key: true
       t.timestamps
