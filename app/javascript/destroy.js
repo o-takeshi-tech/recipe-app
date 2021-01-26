ac63a8e54e9d949e0b24645f5ef1a4fa28e1bf37
o-takeshi-tech <takeshi.film@gmail.com>
2021-01-26 18:57:31 +0900

    JS変更

--- app/javascript/destroy.js
+++ app/javascript/destroy.js
@@ -2,7 +2,7 @@ function destroy (){
   const buttonDelete = document.getElementById("button_delete");
   
   buttonDelete.addEventListener("click", () => {
-    const form = document.querySelectorAll(".input");
+    const form = document.querySelectorAll(".ingredient-form");
     const formNumber = form.length - 1;
     if (formNumber == 0){
       return null 
