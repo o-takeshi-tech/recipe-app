109a9f134924818bb043d57e71e8ad84b9862d32
o-takeshi-tech <takeshi.film@gmail.com>
2021-01-26 17:00:35 +0900

    サーバーサイド変更

--- app/models/recipe_form.rb
+++ app/models/recipe_form.rb
@@ -1,7 +1,7 @@
 class RecipeForm
   include ActiveModel::Model
 
-  attr_accessor :name, :description, :ingredient_name, :user_id
+  attr_accessor :name, :description, :ingredient_names, :user_id
 
   with_options presence: true do
     validates :name
@@ -10,10 +10,12 @@ class RecipeForm
   end
 
   def save
-    binding.pry
-    ingredient = Ingredient.where(ingredient_name: ingredient_name).first_or_initialize
-    ingredient.save
     recipe = Recipe.create(name: name, description: description, user_id: user_id)
-    RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ingredient.id)
+
+    ingredient_names.each do |ingredient_name|
+      ingredient = Ingredient.where(ingredient_name: ingredient_name).first_or_initialize
+      ingredient.save
+      RecipeIngredient.create(recipe_id: recipe.id, ingredient_id: ingredient.id)
+    end
   end
 end
