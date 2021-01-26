0c19b785f7057586e2f66dfd106209ba669aea67
o-takeshi-tech <takeshi.film@gmail.com>
2021-01-26 19:11:43 +0900

    アソシエーションの変更

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,6 +2,7 @@ class Recipe < ApplicationRecord
   belongs_to :user
   has_many  :recipe_ingredients, dependent: :destroy
   has_many  :ingredients, through: :recipe_ingredients
+  has_many  :quantities
   has_one_attached :image
 
 
