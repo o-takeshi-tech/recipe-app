5e673f4ec88ed003cfaef60c87b6cf58121a06dc
o-takeshi-tech <takeshi.film@gmail.com>
2021-02-02 10:18:20 +0900

    サーバーサイド変更

--- app/models/recipe_form.rb
+++ app/models/recipe_form.rb
@@ -1,7 +1,7 @@
 class RecipeForm
   include ActiveModel::Model
 
-  attr_accessor :name, :description, :ingredient_names, :quantities, :user_id
+  attr_accessor :name, :description, :ingredient_names, :quantities, :id, :image, :user_id
 
   with_options presence: true do
     validates :name
@@ -14,20 +14,30 @@ class RecipeForm
     if !(attribute[:id] == nil)
       @recipe = Recipe.find(attribute[:id])
       @ingredients = @recipe.ingredients
-      @quantities = @recipe.quantities
+      @quantity = @recipe.quantities
+      @recipe_ingredients = @recipe.recipe_ingredients
       if attribute[:name] == nil
+        self.id = @recipe.id
         self.name = @recipe.name
         self.description = @recipe.description
-        self.ingredient_names = @ingredients
-        self.quantities = @quantities
         if @recipe.image.present?
           self.image = @recipe.image
         end
+        self.ingredient_names = []
+        self.quantities = []
+        @ingredients.each_with_index do |ingredient, i|
+          self.ingredient_names << ingredient.ingredient_name
+          self.quantities << @quantity[i].quantity
+        end
       else
+        self.id = attribute[:id]
         self.name = attribute[:name]
-        self.image = attribute[:image]
         self.description = attribute[:description]
-        self.ingredients = attribute[:ingredients]
+        self.ingredient_names = attribute[:ingredient_names]
+        self.quantities = attribute[:quantities]
+        if attribute[:image].present?
+          self.image = attribute[:image]
+        end
       end
     else
       super(attribute)
@@ -35,7 +45,11 @@ class RecipeForm
   end
 
   def save
-    recipe = Recipe.create(name: name, description: description, user_id: user_id)
+    if self.image.present?
+      recipe = Recipe.create(name: name, description: description, image: image ,user_id: user_id)
+    else
+      recipe = Recipe.create(name: name, description: description, user_id: user_id)
+    end
     number = 0
     
     ingredient_names.each do |ingredient_name|
@@ -48,7 +62,16 @@ class RecipeForm
   end
 
   def update
-    @recipe.update(name: name, image: image, description: description)
-    
+    if self.image.present?
+      @recipe.update(name: name, image: image, description: description)
+    else
+      @recipe.update(name: name, description: description)
+    end
+    @ingredients.each_with_index do |ingredient, i|
+      ingredient = Ingredient.where(ingredient_name: ingredient_names[i]).first_or_initialize
+      ingredient.update(ingredient_name: ingredient.ingredient_name)
+      @quantity[i].update(quantity: quantities[i], recipe_id: @recipe.id, ingredient_id: ingredient.id)
+      @recipe_ingredients[i].update(recipe_id: @recipe.id, ingredient_id: ingredient.id)
+    end
   end
 end
